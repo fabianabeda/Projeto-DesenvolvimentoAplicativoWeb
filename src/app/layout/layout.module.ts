0f31b71e4cab2d8c14a1a4b79c6329200d76f558
26messias26 <messias26severino@gmail.com>
2022-04-24 17:11:31 -0300

    estilização do header, add do botão favorito, login/perfil e roteamento do botão login

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -12,6 +16,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   imports: [
     CommonModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule
   ],
   exports:[
     HeaderComponent
