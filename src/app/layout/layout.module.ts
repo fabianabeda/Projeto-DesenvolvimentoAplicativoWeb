b65b8f4879112e3411455f32a1d6b179310f7945
26messias26 <messias26severino@gmail.com>
2022-04-24 16:13:30 -0300

    inicio do header

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
-
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
   ],
   exports:[
     HeaderComponent
