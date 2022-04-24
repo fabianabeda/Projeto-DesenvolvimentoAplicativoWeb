a1f57d4aca49bc51844c444bc45c189886192841
fabianabeda <fabiana.beda@gmail.com>
2022-04-23 23:22:29 -0300

    1 file modified, 11 added

---
+++ src/app/usuario/login-usuario/login-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './login-usuario.component.html',
+  styleUrls: ['./login-usuario.component.css']
+})
+export class LoginUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
