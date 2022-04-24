dff7118b51d5877fcb959dfec2a40deecc72e92b
fabianabeda <fabiana.beda@gmail.com>
2022-04-24 13:48:25 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { LayoutModule } from './layout/layout.module';
 import { HomeModule } from './home/home.module';
-import { UsuarioModule } from './usuario/usuario.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
